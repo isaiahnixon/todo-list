b5c26ace617991ae2fd150684c31415341a82c85
Isaiah Nixon <isaiahnixon@gmail.com>
2020-01-16 14:49:14 -0700

    Updates

---
+++ precache-manifest.4674da450afc324ca0f7d228273c7810.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6866b1d545ef707b9790d562cce6b698",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fb5cd3cea5604d110ff",
+    "url": "/static/css/main.def6fd03.chunk.css"
+  },
+  {
+    "revision": "ddc3e6cc2e79b33cda1d",
+    "url": "/static/js/2.623e6fbd.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.623e6fbd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2fb5cd3cea5604d110ff",
+    "url": "/static/js/main.e2b7492a.chunk.js"
+  },
+  {
+    "revision": "b6aab384b0c1e14ae267",
+    "url": "/static/js/runtime-main.d2c0771e.js"
+  }
+]);
